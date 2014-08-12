917356805957bbe9ae6e598bc3a478a57e516a3f
Poisoner <yangwenyi@live.com>
2014-08-12 11:47:56 +0800

    Fix button style

--- SakuraUI.WindowsPhone/Controls/InputTextDialog.xaml.cs
+++ SakuraUI.WindowsPhone/Controls/InputTextDialog.xaml.cs
@@ -19,10 +19,10 @@ namespace SakuraUI.WindowsPhone.Controls
             _service.Opened += (sender, args) => ShowStoryboard.Begin();
             _service.BackKeyPressed += (sender, args) => { args.Handled = true; HideStoryboard.Begin(); };
 
-            ShowStoryboard.Completed += (sender, o) => InputTypeBox.Focus(FocusState.Programmatic);
+            ShowStoryboard.Completed += (sender, o) => InputTextBox.Focus(FocusState.Programmatic);
             HideStoryboard.Completed += (sender, o) => _service.Hide();
 
-            InputTypeBox.KeyDown += (sender, args) => { if (args.Key != VirtualKey.Enter) return; OkOnClick(null, null); };
+            InputTextBox.KeyDown += (sender, args) => { if (args.Key != VirtualKey.Enter) return; OkOnClick(null, null); };
         }
 
         public void Show()
@@ -32,7 +32,7 @@ namespace SakuraUI.WindowsPhone.Controls
 
         public string Title { get { return TitleBlock.Text; } set { TitleBlock.Text = value; } }
         public string Message { get { return MessageBlock.Text; } set { MessageBlock.Text = value; } }
-        public int MaxLength { get { return InputTypeBox.MaxLength; } set { InputTypeBox.MaxLength = value; } }
+        public int MaxLength { get { return InputTextBox.MaxLength; } set { InputTextBox.MaxLength = value; } }
 
         public event EventHandler<string> OkClick;
 
@@ -45,7 +45,7 @@ namespace SakuraUI.WindowsPhone.Controls
         private async void OkOnClick(object sender, RoutedEventArgs e)
         {
             HideStoryboard.Begin();
-            OnOkClick(InputTypeBox.Text.Trim());
+            OnOkClick(InputTextBox.Text.Trim());
         }
 
         private void CancelOnClick(object sender, RoutedEventArgs e)
