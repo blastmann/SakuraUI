6fbfad68521512c2c5f3b1a0e766d77fd2b90d30
Poisoner <yangwenyi@live.com>
2014-08-14 09:52:42 +0800

    Add memory detector for Windows Phone

---
+++ SakuraUI.WindowsPhone/Controls/PhoneMemory.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Windows.System;
+using Windows.UI.Xaml;
+
+namespace SakuraUI.WindowsPhone.Controls
+{
+    public sealed partial class PhoneMemory
+    {
+        public PhoneMemory()
+        {
+            InitializeComponent();
+
+            var timer = new DispatcherTimer { Interval = TimeSpan.FromSeconds(1) };
+            timer.Tick += TimerOnTick;
+            timer.Start();
+        }
+
+        private void TimerOnTick(object sender, object o)
+        {
+            try
+            {
+                var curUsage = MemoryManager.AppMemoryUsage / 1024.0 / 1024.0;
+                var lmtUsage = MemoryManager.AppMemoryUsageLimit / 1024.0 / 1024.0;
+                InfoBlock.Text = string.Format("{0:00.00}/{1}", curUsage, lmtUsage);
+            }
+            catch (Exception ex)
+            {
+                InfoBlock.Text = ex.Message;
+            }
+        }
+    }
+}
