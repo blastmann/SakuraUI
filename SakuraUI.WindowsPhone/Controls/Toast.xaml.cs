1a08c5d042a1195565043f0a5102da3d7bafd1bd
Poisoner <yangwenyi@live.com>
2014-08-14 09:55:29 +0800

    Add toast control for Windows Phone

---
+++ SakuraUI.WindowsPhone/Controls/Toast.xaml.cs
@@ -0,0 +1,34 @@
+﻿using SakuraUI.WindowsPhone.Utilites;
+
+namespace SakuraUI.WindowsPhone.Controls
+{
+    public sealed partial class Toast
+    {
+        private readonly DialogService _service = new DialogService { AnimationType = DialogService.AnimationTypes.Fast };
+
+        public Toast()
+        {
+            InitializeComponent();
+            _service.Child = this;
+            _service.Opened += (sender, args) => PopupMessageStoryboard.Begin();
+            PopupMessageStoryboard.Completed += (sender, o) => _service.Hide();
+        }
+
+        public void Show()
+        {
+            _service.Show();
+        }
+
+        public string Message
+        {
+            get { return MessageText.Text; }
+            set { MessageText.Text = value; }
+        }
+
+        public static void Show(string message)
+        {
+            var toast = new Toast { Message = message };
+            toast.Show();
+        }
+    }
+}
